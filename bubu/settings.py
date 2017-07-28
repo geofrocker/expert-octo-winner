83646ea5bd924357a5fed8488a0e1914b1c4d9d0
geofrocker <geofrocker2@gmail.com>
2017-07-28 20:43:33 +0300

    'hmm'

--- bubu/settings.py
+++ bubu/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
 DATABASES = {
