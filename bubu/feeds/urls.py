8ee316f55be81cdde75cdc46a02228281738c0df
geofrocker <geofrocker2@gmail.com>
2017-07-31 19:24:32 +0300

    'hfff'

--- bubu/feeds/urls.py
+++ bubu/feeds/urls.py
@@ -4,7 +4,7 @@ from bubu.feeds import views
 
 urlpatterns = [
     url(r'^$', views.feeds, name='feeds'),
-    url(r'^products/$', views.post, name='post'),
+    url(r'^post/$', views.post, name='post'),
     url(r'^like/$', views.like, name='like'),
     url(r'^comment/$', views.comment, name='comment'),
     url(r'^load/$', views.load, name='load'),
