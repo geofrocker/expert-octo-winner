194c8f4de3fd893de67f8e39673812b0d16eda0e
geofrocker <geofrocker2@gmail.com>
2017-08-29 18:14:10 +0300

    'hey'

--- bubu/authentication/models.py
+++ bubu/authentication/models.py
@@ -43,7 +43,10 @@ class Profile(models.Model):
             if os.path.isfile(filename):
                 return picture_url
             else:
-                gravatar_url = 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/img/avatar.png'
+                gravatar_url = 'http://www.gravatar.com/avatar/{0}?{1}'.format(
+                    hashlib.md5(self.user.email.lower()).hexdigest(),
+                    urllib.urlencode({'d': no_picture, 's': '256'})
+                    )
                 return gravatar_url
 
         except Exception:
