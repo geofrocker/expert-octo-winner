00517626782cfd5d36136e4b137a91197b78790b
geofrocker <geofrocker2@gmail.com>
2017-09-04 20:06:21 +0300

    'hey'

--- bubu/settings.py
+++ bubu/settings.py
@@ -130,7 +130,7 @@ LANGUAGES = (
 
 LOCALE_PATHS = (PROJECT_DIR.child('locale'), )
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 AWS_ACCESS_KEY_ID =config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY =config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'gelem-static'
